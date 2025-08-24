ad88f900b46d544dbb946e212d5317317d096c72
iquintero24 <i.quintero@civitran.com>
2025-08-24 12:50:56 -0500

    feat: organization of the base model

--- backend/src/models/model.js
+++ backend/src/models/model.js
@@ -2,23 +2,80 @@ import pool from "../utils/pool.js";
 
 /**
  * Base class for database models.
- * Provides access to the shared MySQL connection pool.
+ * Provides access to the shared MySQL connection pool and
+ * common properties like table name and primary key.
  */
 class Model {
   /**
    * Creates a new Model instance.
    *
    * @constructor
-   * @property {Pool} pool - MySQL connection pool instance
+   * @param {string} table - The database table name associated with the model.
+   * @param {string} [primaryKey="id"] - The primary key column of the table.
+   *
+   * @property {import("mysql2/promise").Pool} pool - MySQL connection pool instance.
+   * @property {string} table - Name of the database table.
+   * @property {string} primaryKey - Name of the table’s primary key column.
    */
-  constructor() {
+  constructor(table, primaryKey = "id") {
     /**
      * MySQL connection pool available to child models.
      * Use this to execute queries.
      * @type {import("mysql2/promise").Pool}
      */
     this.pool = pool;
+
+    /**
+     * Database table name associated with the model.
+     * @type {string}
+     */
+    this.table = table;
+
+    /**
+     * Primary key column of the associated table.
+     * @type {string}
+     * @default "id"
+     */
+    this.primaryKey = primaryKey;
   }
+
+  /**Obtener todos los registros */
+  async getAll() {
+    const [rows] = await this.pool.query(`SELECT * FROM ${this.table}`);
+  }
+
+  /**Obtener registro por id*/
+  async getById(id){
+    const [rows] = await this.pool.query(`SELECT * FROM ${this.table} WHERE ${this.primaryKey} = ? LIMIT 1`,
+      [id]
+    );
+    return rows[0] || null
+  }
+
+  /**Crear registro */
+  async create(data){
+    const [result] = await this.pool.query(`INSERT INTO ${this.table} SET ?`, [data]);
+     return { id: result.insertId, ...data }; // return the id insert into table 
+  }
+
+   /** actualizar un registro */
+  async update(id, data) {
+    await this.pool.query(
+      `UPDATE ${this.table} SET ? WHERE ${this.primaryKey} = ?`,
+      [data, id]
+    );
+    return this.getById(id);
+  }
+
+  /** eliminar un registro */
+  async delete(id) {
+    const [result] = await this.pool.query(
+      `DELETE FROM ${this.table} WHERE ${this.primaryKey} = ?`,
+      [id]
+    );
+    return result.affectedRows > 0;
+  }
+
 }
 
 export default Model;
