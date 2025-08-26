2c9135f054104e5a7a9297303ec9e4ede72615b2
keyner barrios <keinerba.ochoa@gmail.com>
2025-08-26 10:29:22 -0500

    fix: router name in adminRouter

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import statusRouter from "./statusRouter.js";
 import AuthenticationRouter from "./authenticationRouter.js";
-import adminRouter from "./adminRouter.js";
+import AdminRouter from "./adminRouter.js";
 
 const router = Router();
 
 router.use("/status", statusRouter)
 router.use("/auth", AuthenticationRouter)
-router.use("/admin",adminRouter)
+router.use("/admin",AdminRouter)
 
 export default router
