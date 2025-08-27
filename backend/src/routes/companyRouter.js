461bf6471d0266c3087b3486bcc77228ad001d77
iquintero24 <i.quintero@civitran.com>
2025-08-27 11:25:11 -0500

    fix: add authorize for company

--- backend/src/routes/companyRouter.js
+++ backend/src/routes/companyRouter.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { validate } from "../middlewares/ValidationMiddleware.js";
 import companyService from "../services/companyService.js";
 import createCompany from "../schemas/company/createCompanySchema.js";
+import { Permissions } from "../utils/roles.js";
+import { authorize } from "../middlewares/AuthorizationMiddleware.js";
 
 
 const companyRouter = Router();
 
-companyRouter.post("/create",validate(createCompany),companyService.createCompany);
+companyRouter.post("/create",authorize(Permissions.COMPANY.CREATE ),validate(createCompany),companyService.createCompany);
 
 export default companyRouter;
