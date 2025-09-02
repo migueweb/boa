4b93810152db1c453e6f456a082f7fe8f46e6c45
Keiner Barrios <keinerba.ochoa@gmail.com>
2025-09-02 17:50:24 -0500

    Creation of the user module, the route was added in routes and the necessary services were raised: companyServices, roleService, userServices in the backend and the front

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -1,19 +1,7 @@
-
 import { request, response } from "express";
 import userModel from "../models/userModel.js";
 
-/**
- * Service class responsible for managing the logic of company creation.
- */
 export default class UsersService {
-  /**
-   * Handles the creation of an users.
-   *
-   * @async
-   * @param {request} req - Express request object containing admin data in `req.body`.
-   * @param {response} res - Express response object used to send success or error responses.
-   */
-
   static async get(req, res) {
     try {
       const users = await userModel.getAll();
@@ -22,11 +10,43 @@ export default class UsersService {
         message: "users retrieved successfully",
         data: users,
       });
-      
     } catch (error) {
       console.error("Error getting users:", error);
       return res.error("Internal server error", 500);
     }
   }
 
+  static async create(req, res) {
+    try {
+      console.log("Received data:", req.body);
+
+      const { email } = req.body;
+
+
+      const existingUser = await userModel.getByEmail(email);
+      if (existingUser) {
+        return res.error("User already exists", 409);
+      }
+
+
+      console.log("Creating user...");
+      await userModel.createUser(req.body);
+
+
+      const createdUser = await userModel.getByEmail(email);
+      if (!createdUser) {
+        return res.error("Error creating user", 500);
+      }
+
+      console.log("User created successfully");
+      return res.success({
+        message: "User created successfully",
+        data: createdUser
+      }, 201);
+
+    } catch (error) {
+      console.error("UsersService.create error:", error);
+      return res.error("Internal server error", 500);
+    }
+  }
 }
--- frontend/src/js/routes.js
+++ frontend/src/js/routes.js
@@ -2,6 +2,7 @@ import Home from "./pages/Home.js";
 import Login from "./pages/Login.js";
 import Dashboard from "./pages/Dashboard.js"
 import ReservationsPage from "./pages/reservation.js";
+import UserDashboard from "./pages/user.js";
 
 
 /**
@@ -16,6 +17,7 @@ const routes = {
   "/login": { view: Login, auth: false, guest: true },
   "/dashboard": { view: Dashboard, auth: true, /* permissions: ["dashboard_access"]  */},
   "/reservations": {view: ReservationsPage, auth: true },
+  "/user": { view: UserDashboard, auth: true},
   "/404": { view: () => "<h1>404 Not Found</h1>", auth: false },
 };
 
