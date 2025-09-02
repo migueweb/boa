9b0b4687131af3cfbcefb6932f938a1fc8b8d102
Miguel Amador <miguesdev@gmail.com>
2025-09-02 09:19:11 -0500

    docs(frontend): project and workflow documen documented

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -7,20 +7,17 @@ import router from './router';
 
 const app = document.querySelector("#app")
 
-/**
- * Managing anchors [data-route] for spa navigation
-*/
+
 document.addEventListener("click", (e) => {
-  if (e.target.matches("a")) return anchorHandler(e)
-  if (e.target.matches("#logoutBtn")) return logoutHandler()
+  if (e.target.matches("a")) return anchorHandler(e)  //handling anchors for spa navigation
+  if (e.target.matches("#logoutBtn")) return logoutHandler() // handling logout btn
 });
 
 
 app.addEventListener("submit", async (e) => {
   e.preventDefault()
   
-  if (e.target.matches("#loginForm")) return await loginFormHandler(e)
-
+  if (e.target.matches("#loginForm")) return await loginFormHandler(e) // login handler
   
 })
 
